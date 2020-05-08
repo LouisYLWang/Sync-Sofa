f08a3692f0c65c2f3f0e04c5e29cb0fdfe5b850b
Onns <onns@onns.xyz>
2020-05-09 00:14:24 +0800

    Add some feature

--- client/chrome_extension/content.js
+++ client/chrome_extension/content.js
@@ -2,86 +2,126 @@ const PAUSECODE = "1";
 const PLAYCODE = "2";
 const CLOSEDCODE = "-1";
 const DISCONNECTCODE = "-2";
-const apihost = "app.ylwang.me"
+const SYNCCODE = "3";
+const apihost = "api2.ylwang.me"
 const STATUSSTART = "start"
 const STATUSEND = "end"
 const STATUSSYNC = "sync"
+const STATUSASK = "ask"
+
+var status = STATUSEND;
+var websocket = null;
+
+// check connection every 30s.
+setInterval(function () {
+    isOpen(websocket) ? status = STATUSSYNC : status = STATUSEND;
+}, 1000 * 30);
 
 
-chrome.runtime.onMessage.addListener((msg)=> {
-  if (msg.status === STATUSSTART){
-      console.log(`RECEIVED sessionID ${msg.body}`);
-      let url = `wss://${apihost}/ws/?id=${msg.body}`;
-      var websocket = new WebSocket(url);
-      var video = document.querySelectorAll('video')[0]  
-      handleOnSessions(websocket, video);
-  }
-});
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
 
-function isOpen(websocket) { return websocket.readyState === websocket.OPEN }
-
-function handleOnSessions(websocket, video){
-  video.addEventListener("pause", (e)=>{
-    e.stopPropagation();
-    if (isOpen(websocket)){
-      websocket.send(PAUSECODE)
-      console.log(`SENT PAUSECODE`);
-      return;
-    };
-  })
-
-  video.addEventListener("play", (e)=>{
-    e.stopPropagation();
-    if (isOpen(websocket)){
-      websocket.send(video.currentTime)
-      console.log(`SENT CURRENT TIME`);
-      websocket.send(PLAYCODE)
-      console.log(`SENT PLAYCODE`);
-      return;
-    };
-  })
-
-  video.onseeking= function() {
-    websocket.send(video.currentTime)
-    console.log(`SENT CURRENT TIME`);
-    return;
-  }
-
-  websocket.onmessage = (msg) => {
-    switch (msg.data) {
-      case CLOSEDCODE:
-        video.pause();
-        alert("socket connection closed by other partner");
-        console.log("socket connection closed by other partner");
-        websocket.close();
-        return;
-      case DISCONNECTCODE:
-        video.pause();
-        alert("not connected to other partner");
-        websocket.close();
-        return;
-      case PAUSECODE: 
-        console.log(`RECEIVED PAUSECODE`);
-        if(!video.paused){
-          setTimeout(function () {
-            video.pause();   
-        }, 500);
+        if (request.status === STATUSSTART) {
+            console.log(`RECEIVED sessionID ${request.body}`);
+            let url = `wss://${apihost}/ws/?id=${request.body}`;
+            if (websocket) { websocket.close(); }
+            websocket = new WebSocket(url);
+            var video = document.querySelectorAll('video')[0];
+            status = STATUSSYNC;
+            handleOnSessions(websocket, video);
         }
-        return;
-      case PLAYCODE:
-        console.log(`RECEIVED PLAYCODE`);
-        if(video.paused){
-          video.play();
+        
+        if (request.status === STATUSSYNC) {
+            websocket.send(SYNCCODE);
+            console.log(`SENT SYNCCODE`);
         }
-        return;
-      default:
-        if(msg.data <= video.duration && msg.data >= 0){
-          if (Math.abs(msg.data - video.currentTime) > 1){
-            video.currentTime = msg.data;
-            console.log(`RECEIVED CURRENT TIME`);
-          }
+
+        if (request.status === STATUSEND) {
+            websocket.send(CLOSEDCODE);
+            console.log(`SENT CLOSEDCODE`);
+            console.log("socket connection closed");
+            websocket.close();
+            status = STATUSEND;
         }
+
+        if (request.status == STATUSASK) {
+            console.log("ask for status: " + status);
+            sendResponse({ "status": STATUSASK, "body": status });
+        }
+
+    });
+
+function isOpen(websocket) {
+    return websocket !== null && websocket.readyState === websocket.OPEN;
+}
+
+function handleOnSessions(websocket, video) {
+    video.addEventListener("pause", (e) => {
+        e.stopPropagation();
+        if (isOpen(websocket)) {
+            websocket.send(PAUSECODE)
+            console.log(`SENT PAUSECODE`);
+            return;
+        };
+    })
+
+    video.addEventListener("play", (e) => {
+        e.stopPropagation();
+        if (isOpen(websocket)) {
+            websocket.send(video.currentTime)
+            console.log(`SENT CURRENT TIME`);
+            websocket.send(PLAYCODE)
+            console.log(`SENT PLAYCODE`);
+            return;
+        };
+    })
+
+    video.onseeking = function () {
+        websocket.send(video.currentTime)
+        console.log(`SENT CURRENT TIME`);
         return;
     }
-  }
+
+    websocket.onmessage = (msg) => {
+        switch (msg.data) {
+            case CLOSEDCODE:
+                video.pause();
+                alert("socket connection closed by other partner");
+                console.log("socket connection closed by other partner");
+                websocket.close();
+                status = STATUSEND;
+                return;
+            case DISCONNECTCODE:
+                video.pause();
+                alert("not connected to other partner");
+                websocket.close();
+                status = STATUSEND;
+                return;
+            case PAUSECODE:
+                console.log(`RECEIVED PAUSECODE`);
+                if (!video.paused) {
+                    setTimeout(function () {
+                        video.pause();
+                    }, 500);
+                }
+                return;
+            case PLAYCODE:
+                console.log(`RECEIVED PLAYCODE`);
+                if (video.paused) {
+                    video.play();
+                }
+                return;
+            default:
+                if (msg.data <= video.duration && msg.data >= 0) {
+                    if (Math.abs(msg.data - video.currentTime) > 1) {
+                        video.currentTime = msg.data;
+                        console.log(`RECEIVED CURRENT TIME`);
+                    }
+                }
+                return;
+        }
+    }
 }
