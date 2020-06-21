6a58b0d5f06697d6f2aebc4b56a896cc9a0c53aa
Onns <onns@onns.xyz>
2020-06-21 17:48:46 +0800

    add shake animation

--- client/chrome_extension/content.js
+++ client/chrome_extension/content.js
@@ -242,6 +242,28 @@ class chat {
             border: 6px solid transparent;
             border-right-color: #cccac7;
           }
+        @-webkit-keyframes chat-shake {
+            0% { -webkit-transform: translate(2px, 1px) rotate(0deg); } 
+            10% { -webkit-transform: translate(-1px, -2px) rotate(-1deg); }
+            20% { -webkit-transform: translate(-3px, 0px) rotate(1deg); }
+            30% { -webkit-transform: translate(0px, 2px) rotate(0deg); }
+            40% { -webkit-transform: translate(1px, -1px) rotate(1deg); }
+            50% { -webkit-transform: translate(-1px, 2px) rotate(-1deg); }
+            60% { -webkit-transform: translate(-3px, 1px) rotate(0deg); }
+            70% { -webkit-transform: translate(2px, 1px) rotate(-1deg); }
+            80% { -webkit-transform: translate(-1px, -1px) rotate(1deg); }
+            90% { -webkit-transform: translate(2px, 2px) rotate(0deg); }
+            100% { -webkit-transform: translate(1px, -2px) rotate(-1deg); }
+        }
+        .chat-shake {
+            -webkit-animation-name: chat-shake;
+            -webkit-animation-duration: 0.5s;
+            -webkit-transform-origin:50% 50%;
+            -webkit-animation-iteration-count: infinite;
+        }
+        .chat-shake {
+            display:inline-block
+        }
           
         `;
         document.head.appendChild(chatButtonStyle);
@@ -316,6 +338,9 @@ class chat {
     }
     notification() {
         this.chatButton.style.backgroundColor = "#e13b3b";
+
+        // css animation from https://stackoverflow.com/a/36964181/13182099
+        this.chatButton.className += ' chat-shake';
     }
 
     renderMessage(message, type) {
