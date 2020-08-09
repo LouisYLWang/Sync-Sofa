ad7025cbea34b331974ba5ac9ec3cef0263466aa
LouisYLWang <louis.yl.wang@outlook.com>
2020-08-09 00:28:26 -0700

    fix empty code bug

--- server/handlers/socketHandler.go
+++ server/handlers/socketHandler.go
@@ -24,16 +24,12 @@ func (ctx *Context) InserConnection(id session.SessionID, conn *websocket.Conn) 
 	defer ctx.SocketStore.Lock.Unlock()
 	ctx.SocketStore.ConnectionsMap[id] = conn
 	log.Println("socket connected to session: ", id)
-
 }
 
 func (ctx *Context) RemoveConnection(sessionID session.SessionID, conn *websocket.Conn) {
 	ctx.SocketStore.Lock.Lock()
 	defer ctx.SocketStore.Lock.Unlock()
-	partnerID, err := GetPartnerID(sessionID)
-	if err != nil {
-		return
-	}
+	partnerID := GetPartnerID(sessionID)
 	partnerConn, exist := ctx.SocketStore.ConnectionsMap[partnerID]
 	if exist {
 		log.Printf("partner cient %s been force closed\n", sessionID)
@@ -44,11 +40,11 @@ func (ctx *Context) RemoveConnection(sessionID session.SessionID, conn *websocke
 	log.Println("socket remove connection to session: ", sessionID)
 }
 
-func GetPartnerID(selfID session.SessionID) (session.SessionID, error) {
+func GetPartnerID(selfID session.SessionID) (session.SessionID) {
 	if (len(selfID) != 5) {
-		return session.InvalidSessionID, fmt.Errorf("incorrect sessionID length!")
+		fmt.Print("here")
+		return session.InvalidSessionID
 	}
-
 	usrID := selfID[len(selfID)-1 : len(selfID)]
 	pairID := selfID[0 : len(selfID)-1]
 	var partnerID session.SessionID
@@ -59,20 +55,22 @@ func GetPartnerID(selfID session.SessionID) (session.SessionID, error) {
 		partnerID = pairID + "0"
 	} else {
 		log.Printf("error parsing the partnerID\n")
-		return session.InvalidSessionID, nil
+		return session.InvalidSessionID
 	}
-	return partnerID, nil
+	return partnerID
 }
 
-func GetPairID(selfID session.SessionID) session.SessionID {
+func GetPairID(selfID session.SessionID) (session.SessionID) {
 	return selfID[0 : len(selfID)-1]
 }
 
-
 func (ctx *Context) WebSocketConnHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
 	sessionID := session.SessionID(r.URL.Query().Get(paramID))
-
+	if (len(sessionID) != 5) {
+		http.Error(w, "failed to open websocket connection", http.StatusUnauthorized)
+		return
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		http.Error(w, "failed to open websocket connection", http.StatusUnauthorized)
 		return
@@ -93,12 +91,7 @@ func (ctx *Context) WebSocketConnHandler(w http.ResponseWriter, r *http.Request)
 			}
 			log.Printf("client %s operate \n", id)
 			//conn.WriteMessage(websocket.TextMessage, p)
-			partnerID, err:= GetPartnerID(id)
-			if err != nil {
-				http.Error(w, "failed to open websocket connection", http.StatusUnauthorized)
-				return
-			}
-
+			partnerID:= GetPartnerID(id)
 
 			if messageType == websocket.TextMessage {
 				partnerConn, exist := ctx.SocketStore.ConnectionsMap[partnerID]
